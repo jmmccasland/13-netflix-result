c5980a77822db9398cec9622aaeb05c32ad3d4cd
John Michael McCasland <jmmccasland@gmail.com>
2017-02-20 16:08:12 -0600

    JS SetUp

--- app/index.js
+++ app/index.js
@@ -1,1 +1,6 @@
 import 'whatwg-fetch';
+
+// Search Variables
+const searchInput = document.querySelector('.card__search-input');
+const searchBtn = document.querySelector('.card__search-btn');
+const searchResult = document.querySelector('.card__results');
