4ff347d69a978f1b840e9a3ef58b752240320b59
John Michael McCasland <jmmccasland@gmail.com>
2017-03-27 15:27:20 -0500

    project complete

--- app/index.js
+++ app/index.js
@@ -7,15 +7,17 @@ const searchInput = document.querySelector('.card__search-input');
 const searchBtn = document.querySelector('.card__search-btn');
 const searchResult = document.querySelector('.card__results');
 
-  function searchForTitle(name) {
-    fetch(`http://netflixroulette.net/api/api.php?title=${name}`)
-      .then((res) => {
-        return res.json();
-      }).then((data) => {
+
+function searchForTitle(name) {
+  fetch(`http://netflixroulette.net/api/api.php?title=${name}`)
+      .then(res => res.json()).then((data) => {
         fillInResult(searchResult, data);
       });
-  }
+}
+
 
 searchBtn.addEventListener('click', () => {
   searchForTitle(searchInput.value);
 });
+
+searchForTitle('Breaking Bad');
